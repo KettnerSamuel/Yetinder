2f1f0c57771a77b7674e222d51e652314dc581a3
Samuel Kettner <Kettners21@spstrutnov.cz>
2024-05-21 20:42:59 +0200

    doma ...

--- App/migrations/Version20240521175020.php
+++ App/migrations/Version20240521175020.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240521082511 extends AbstractMigration
+final class Version20240521175020 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240521082511 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE rating_history ADD rating LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', DROP rating_up, DROP rating_down');
+        $this->addSql('ALTER TABLE rating_history ADD rating LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', DROP date, DROP up, DROP down');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE rating_history ADD rating_down LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', CHANGE rating rating_up LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE rating_history ADD date DATE NOT NULL, ADD up INT DEFAULT NULL, ADD down INT DEFAULT NULL, DROP rating');
     }
 }
